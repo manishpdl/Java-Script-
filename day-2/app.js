2c3e820b41b538ea832498e6feaa4814dc8e2d57
Manish Poudel <88720680+manishpdl@users.noreply.github.com>
2024-05-30 22:33:37 +0545

    Practise on array and string method

---
+++ day-2/app.js
@@ -0,0 +1,21 @@
+//1
+// let msg="help!";
+// console.log(msg.trim().toUpperCase());
+
+
+//2
+// let name="ManishPoudel";
+// console.log(name.slice(4,9)); //shpou
+// console.log(name.indexOf("ou"));//7
+// console.log(name.replace("Manish","manish"));
+// console.log(name.slice(6).replace('P','D'));
+
+//3
+// let start=['jan','july','march','august'];
+// console.log(start.shift());
+// console.log(start.shift());
+// console.log(start.unshift('june'));
+// console.log(start.unshift('july'));
+
+
+// console.log(start);

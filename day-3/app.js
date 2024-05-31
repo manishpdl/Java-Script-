e2c830fad865bab98e160f4e9f6bf16452527f98
Manish Poudel <88720680+manishpdl@users.noreply.github.com>
2024-05-31 20:52:02 +0545

    Array methods

---
+++ day-3/app.js
@@ -0,0 +1,78 @@
+
+//qn1
+
+// let start=['jan','july','march','august'];
+// console.log(start);
+// console.log(start.splice(0,2));
+// console.log(start.splice(0,0,'july','june'));
+// let final=start
+// console.log(final);
+
+//qn2
+
+// let program=['c','c++','javascript','html','python','java','c#','sql'];
+// console.log(program);
+// console.log(program.reverse().indexOf('javascript'));
+// console.log(program);
+
+//qn3
+// let game=[['X',null,'0'],[null,'X',null],['0',null,'X']];
+// console.log(game);
+// console.log(game[0][1]='0');
+
+//qn4
+//  Write a JavaScript program to get the first n elements of an array. [n can be any
+//   positive number].
+//   For example: for array [7, 9, 0,-2] and n=3
+// Print, [7, 9, 0]
+
+// let array=[7,9,0,-2];
+// let n=3;
+// let ans=array.splice(0,n);
+// console.log(ans);
+
+
+//qn5
+// let array=[7,9,0,-2];
+// console.log(array.length);
+// let n=3;
+// console.log(array.slice(array.length-n))
+
+
+//  Qs6. Write a JavaScript program to check whether a string is blank or not.
+// let string=prompt("enter any string")
+// if(string.length==0)
+//     {
+//         console.log("empty")
+//     }
+//     else{
+//         console.log(`not emppty: ${string}`);
+//     }
+
+// Write a JavaScript program to test whether the character at the given (character)
+//  index is lower case.
+
+// var str="MaNiSh Poudel";
+// var idx=3;
+// if(str[idx]==str[idx].toUpperCase()){
+//     console.log("uppercase");
+// }
+// else{
+//     console.log("lowercase");
+// }
+
+// Write a JavaScript program to strip leading and trailing spaces from a string
+// let str="  manish    "
+// console.log(str.trim());
+
+//Write a JavaScript program to check if an element exists in an array or not
+// let number=[1,2,3,4,5,6];
+// let element=6;
+// if(number.includes(element)){
+//     console.log("the element lies in array ");
+// }
+// else
+// console.log("the element  doesnot lies in array ");
+// {
+
+// }

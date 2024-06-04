ee1fed0784a0fba446c5f55335959405a758c7ce
Manish Poudel <88720680+manishpdl@users.noreply.github.com>
2024-06-04 16:48:54 +0545

    day-5
    Factorial,Sum and counting

---
+++ day-5/app.js
@@ -0,0 +1,41 @@
+// let num=4;
+// let fact=1;
+// for(let i=1;i<=num;i++)
+// {
+//     fact=fact*i;
+
+// }
+// console.log(fact);
+
+
+
+
+
+// let number = 123456; // Example number
+// let str=number.toString();
+// let sum=0;
+// for(i=0;i<str.length-1;i++){
+//    sum=sum+ str(i);
+// }
+
+// // sum ofthe digit of the number
+
+// let num = 123456;
+// let sum = 0;
+// while (num > 0) {
+//   let digit = num % 10; //6//5
+//   sum = sum + digit; //6
+//   num = Math.floor(num / 10);
+// }
+// console.log(sum); 
+// count
+// let str="123456";
+// let num=parseInt(str,10);
+// count=0;
+// while(num>0){
+// let digit=num%10;//6//5//4//3//2//1
+// count=count+1;//1//2//3//4//5//6
+// num=Math.floor(num/10);//12345,1234,123,12,1,0
+
+// }
+// console.log(count);

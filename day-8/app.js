27ab386c1f44115e7f4b8a0a18d65f8d5b52dea4
Manish Poudel <88720680+manishpdl@users.noreply.github.com>
2024-06-08 22:09:47 +0545

    adding some practice qn

--- day-8/app.js
+++ day-8/app.js
@@ -179,3 +179,73 @@
 //     return ans;
 // }
 // console.log(uniquechar(str));
+
+
+
+// Write a JavaScript function that accepts a list of country names as input and 
+// returns the longest country name as output. 
+// Example : country = ["Australia", "Germany", "United States of America"] output : 
+// "United States of America" 
+
+
+// let country = ["Australia", "Germany", "United States of America"];
+// let longestcountry='';
+// for(i=0;i<country.length;i++)
+//     {
+//    if(country[i].length>longestcountry.length)
+//     {
+//         longestcountry=country[i];
+//     }
+
+//            }
+//  console.log(longestcountry); 
+
+// let country = ["Australia", "United States of America", "Germany","Nepal"];
+// let lengthcountry='';
+
+// function longestcountryname(country)
+// {
+//     for(let i=0;i<country.length;i++)
+//     {
+//       if( country[i].length>lengthcountry.length){
+//         lengthcountry=country[i];}
+//     }
+    
+//     return lengthcountry;
+    
+// }
+// console.log(longestcountryname(country));
+
+
+// qn. Write a JavaScript function to count the number of vowels in a String 
+// argument.
+
+// let str = "mAnish";
+
+// function findVowel(str) {
+//     let count = 0; // Initialize count inside the function
+
+//     // Convert the string to lowercase to handle uppercase vowels
+//     str = str.toLowerCase();
+
+//     for (let i = 0; i < str.length; i++) {
+//         // Check if the character is a vowel
+//         if (str.charAt(i) === 'a' || str.charAt(i) === 'e' || str.charAt(i) === 'i' || str.charAt(i) === 'o' || str.charAt(i) === 'u') {
+//             count += 1;
+//         }
+//     }
+
+//     return count;
+// }
+
+// console.log(findVowel(str)); // Output: 2
+
+
+
+//qn random number in range
+
+// function randomnumber(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// console.log(randomnumber(1, 10)); // Output: A random number between 1 and 10 (inclusive)
